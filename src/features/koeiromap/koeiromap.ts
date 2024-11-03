d4257ec35fc665d8d90503e5733542cdbbf8ebb2
torimoti <take1010ct@gmail.com>
2024-11-03 22:02:16 +0900

    Update koeiromap.ts

--- src/features/koeiromap/koeiromap.ts
+++ src/features/koeiromap/koeiromap.ts
@@ -5,12 +5,12 @@ import { TalkStyle } from "../messages/messages";
 export async function voicevox(message: string, talkStyle?: TalkStyle) {
     if (talkStyle === undefined || talkStyle == null){
     } else {
-        message = '[' + talkStyle.toString() + ']' + message;
+        // message = '[' + talkStyle.toString() + ']' + message;
     }
 
     const query = await fetch(
         'http://127.0.0.1:50021/audio_query'
-        + '?speaker=3&text=' + encodeURIComponent(message),
+        + '?speaker=8&text=' + encodeURIComponent(message),
         {
             method: 'POST',
         }
@@ -23,7 +23,7 @@ export async function voicevox(message: string, talkStyle?: TalkStyle) {
     const tts_json = await query.json();
     const response = await fetch(
         'http://127.0.0.1:50021/synthesis'
-        + '?speaker=3',
+        + '?speaker=8',
         {
             method: 'POST',
             headers: {
@@ -63,67 +63,3 @@ export async function koeiromapFreeV1(
   const ret = voicevox(message, style);
   return ret;
 }
-
-
-// export async function koeiromapV0(
-//   message: string,
-//   speakerX: number,
-//   speakerY: number,
-//   style: TalkStyle
-// ) {
-//   const param = {
-//     method: "POST",
-//     body: JSON.stringify({
-//       text: message,
-//       speaker_x: speakerX,
-//       speaker_y: speakerY,
-//       style: style,
-//     }),
-//     headers: {
-//       "Content-type": "application/json; charset=UTF-8",
-//     },
-//   };
-
-//   const koeiroRes = await fetch(
-//     "https://api.rinna.co.jp/models/cttse/koeiro",
-//     param
-//   );
-
-//   const data = (await koeiroRes.json()) as any;
-
-//   return { audio: data.audio };
-// }
-
-// export async function koeiromapFreeV1(
-//   message: string,
-//   speakerX: number,
-//   speakerY: number,
-//   style: "talk" | "happy" | "sad",
-//   apiKey: string
-// ) {
-//   // Request body
-//   const body = {
-//     text: message,
-//     speaker_x: speakerX,
-//     speaker_y: speakerY,
-//     style: style,
-//     output_format: "mp3",
-//   };
-
-//   const koeiroRes = await fetch(
-//     "https://api.rinna.co.jp/koeiromap/v1.0/infer",
-//     {
-//       method: "POST",
-//       body: JSON.stringify(body),
-//       headers: {
-//         "Content-Type": "application/json",
-//         "Cache-Control": "no-cache",
-//         "Ocp-Apim-Subscription-Key": apiKey,
-//       },
-//     }
-//   );
-
-//   const data = (await koeiroRes.json()) as any;
-
-//   return { audio: data.audio };
-// }
